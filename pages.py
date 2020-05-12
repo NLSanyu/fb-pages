46d0b33e8c74cce76c1d7dda81662a7bcbe08225
NLSanyu <lydiansanyu@gmail.com>
2020-05-12 10:26:07 +0300

    Fetch posts

--- pages.py
+++ pages.py
@@ -2,7 +2,12 @@ from decouple import config
 import facebook
 
 page_token = config('PAGE_ACCESS_TOKEN')
+page_id = config('PAGE_ID')
+
 graph = facebook.GraphAPI(access_token=page_token, version="3.1")
 print(graph)
-page_info = graph.get_object(id=105141914536236)
+page_info = graph.get_object(id=page_id, fields='about, website')
 print(page_info)
+posts = graph.get_object(id=page_id,
+                        fields='posts.limit(100)')
+print(posts)
